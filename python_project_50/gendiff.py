61267f68051863f258ab1fc5a725c0bf02a2c6eb
NikolayZemelko <zemelko1989@gmail.com>
2022-10-12 13:04:09 +0300

    create gendiff

---
+++ python_project_50/gendiff.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+
+import argparse
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('file_1', metavar='first_file')
+    parser.add_argument('file_2', metavar='second_file')
+
+    args = parser.parse_args()
+    print(args.accumulate(args.file_1, args.file_2))
+
+if __name__ == '__main__':
+    main()
