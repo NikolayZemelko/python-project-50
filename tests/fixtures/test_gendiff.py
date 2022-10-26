fca01488b930af88f101ec2a9bdf20c3bc32207c
NikolayZemelko <zemelko1989@gmail.com>
2022-10-26 21:05:44 +0300

    Create tests

--- tests/fixtures/test_gendiff.py
+++ tests/fixtures/test_gendiff.py
@@ -1,1 +1,28 @@
-from gendiff import generate_diff
+from gendiff.scripts.gendiff import generate_diff
+import pytest
+
+@pytest.fixture
+def files():
+    return [
+    {
+      "host": "hexlet.io",
+      "timeout": 50,
+      "proxy": "123.234.53.22",
+      "follow": "false"
+    },
+    {
+      "timeout": 20,
+      "verbose": "true",
+      "host": "hexlet.io"
+    }
+]
+
+def test_if_first_file_empty(files):
+    file1 = dict()
+    file2 = files[1]
+
+    assert generate_diff(file1, file2) == """{
+    host: hexlet.io
+    timeout: 20
+    verbose: true
+}"""
